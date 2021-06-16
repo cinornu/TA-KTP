d7b5e66f6756685eca7e34b664462ea5a602fa5c
cinornu <uueeck@gmail.com>
2021-06-16 22:03:26 +0900

    init

---
+++ init.lua
@@ -0,0 +1,31 @@
+-- $Id: init.lua 4213 2021-05-25 23:21:31Z dsb $
+-- ToME - Tales of Maj'Eyal
+-- Copyright © 2012-2021 Scott Bigham
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Scott Bigham ("Zizzo")
+-- dsb-tome@killerbunnies.org
+
+long_name = "ToME4 Addons Korean Translation Pack"
+short_name = "ta-ktp"
+for_module = "tome"
+version = { 1, 7, 4 }
+addon_version = { 1, 0, 0 }
+weight = 255
+author = { 'cinornu', 'uueeck@gmail.com' }
+homepage = ''
+description = [[ToME4 애드온 통합 한국어 번역 패키지입니다.]]
+tags = { 'translation' }
+data = true
